816c792230b7cf159e5431fcaa94b39e6d2e8618
Jonathan <jonathan.j.cannon@gmail.com>
2019-11-07 10:13:05 -0500

    Up to date?
    Yuppp

--- make_all_metronomes.m
+++ make_all_metronomes.m
@@ -1,7 +1,7 @@
 all_periods = [.6, .7, .8];
 all_filewords = {'600', '700', '800'};
-mkdir(strcat('stimulus_sequences/', datestr(clock, 'dd-mmm-yyyy HH+MM+SS')));
-filepath = strcat('stimulus_sequences/', datestr(clock, 'dd-mmm-yyyy HH+MM+SS'), '/');
+mkdir(strcat('../stimulus_sequences/', datestr(clock, 'dd-mmm-yyyy HH+MM+SS')));
+filepath = strcat('../stimulus_sequences/', datestr(clock, 'dd-mmm-yyyy HH+MM+SS'), '/');
 
 params = struct();
 
@@ -13,14 +13,14 @@ params.wav_separate = false;
 params.Fs = 44100;
 params.sound_list = {};
 
-tick = audioread('stimulus_components/wood_tick.wav');
+tick = audioread('../stimulus_components/wood_tick.wav');
 params.sound_list{1} = 0.2*tick(:,1);
 params.standard_index = 1;
 
 params.sound_list{2} = .2*resample(tick(:,1), 4,5);
 params.deviant_index = 2;
 
-target = audioread('stimulus_components/target_beep.wav');
+target = audioread('../stimulus_components/target_beep.wav');
 params.sound_list{3} = 0.2*target(:,1);
 params.target_index = 3;
 
@@ -41,33 +41,39 @@ tapping_blocks = {};
 separation_space = zeros(44100*5, 2);
 separation_space(:,2)=params.intertrial_amplitude;
 
-rxn_inst_1 = audioread('stimulus_components/ThisIsTargetSound.wav');
-rxn_inst_2 = audioread('stimulus_components/TapReaction.wav');
-rxn_inst_3 = audioread('stimulus_components/DoNotMove.wav');
+rxn_inst_1 = 1.2*audioread('../stimulus_components/ThisIsTargetSound.wav');
+rxn_inst_2 = audioread('../stimulus_components/TapReaction2.wav');
+rxn_inst_3 = audioread('../stimulus_components/DoNotMove.wav');
 
-init_rxn_inst = vertcat(rxn_inst_1, params.sound_list{params.target_index});
+free_inst = audioread('../stimulus_components/FreeTap.wav');
+
+init_rxn_inst = vertcat(rxn_inst_1, params.sound_list{params.target_index}, rxn_inst_2, rxn_inst_3, zeros(44100*2, 1));
 init_rxn_inst(:,2) = 0;
-rxn_inst = vertcat(rxn_inst_2, rxn_inst_3, zeros(44100*2, 1));
+rxn_inst = vertcat(rxn_inst_2, zeros(44100*2, 1));
 rxn_inst(:,2) = 0;
 
-tapalong_inst = vertcat(audioread('stimulus_components/TapAlong.wav'), zeros(44100*1, 1));
+tapalong_inst = vertcat(audioread('../stimulus_components/TapAlong.wav'), zeros(44100*1, 1));
 tapalong_inst(:,2) = 0;
 
 for i = 1:length(all_periods)
 
-    block = stim_maker_standard(strcat(filepath, 'listen_metronome_', all_filewords{i}, '_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), all_periods(i), 45+floor(rand()*5), true, i, params);
-    block.instruction_type = 'listen';
-    block.instructions = rxn_inst;
-    listening_blocks{end+1} = block;
+    for j=1:2
+        block = stim_maker_standard(strcat(filepath, 'listen_metronome_', all_filewords{i}, '_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), all_periods(i), 25+floor(rand()*5), true, i, params);
+        block.instruction_type = 'listen';
+        block.instructions = rxn_inst;
+        listening_blocks{end+1} = block;
+    end
     
     
-    block = stim_maker_standard(strcat(filepath, 'tap_metronome_', all_filewords{i}, '_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), all_periods(i), 45+floor(rand()*5), false, i, params);
-    block.instruction_type = 'tap';
-    block.instructions = tapalong_inst;
-    tapping_blocks{end+1} = block;
+    for j=1:3
+        block = stim_maker_standard(strcat(filepath, 'tap_metronome_', all_filewords{i}, '_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), all_periods(i), 25+floor(rand()*5), false, i+10, params);
+        block.instruction_type = 'tap';
+        block.instructions = tapalong_inst;
+        tapping_blocks{end+1} = block;
+    end
     
     for j = 1:4
-        block = stim_maker_deviant(strcat(filepath, 'listen_deviant_metronome_', j, '_', all_filewords{i}, '_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), all_periods(i), 25+floor(rand()*5), 0.3, true, i, params);
+        block = stim_maker_deviant(strcat(filepath, 'listen_deviant_metronome_', j, '_', all_filewords{i}, '_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), all_periods(i), 25+floor(rand()*5), 0.3, true, i+20, params);
         block.instruction_type = 'listen';
         block.instructions = rxn_inst;
         listening_blocks{end+1} = block;
@@ -81,17 +87,18 @@ for i = 1:length(all_periods)
 %     tapping_blocks{end}.params = params;
     
     for j = 1:4
-        block = stim_maker_omission(strcat(filepath, 'listen_omission_metronome_', j, '_', all_filewords{i}, '_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), all_periods(i), 25+floor(rand()*5), 0.3, true, i, params);
+        block = stim_maker_omission(strcat(filepath, 'listen_omission_metronome_', j, '_', all_filewords{i}, '_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), all_periods(i), 25+floor(rand()*5), 0.3, true, i+30, params);
         block.instruction_type = 'listen';
         block.instructions = rxn_inst;
         listening_blocks{end+1} = block;
     end
     
-    block = stim_maker_omission(strcat(filepath, 'tap_omission_metronome_', all_filewords{i}, '_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), all_periods(i), 45+floor(rand()*5), 0.3, false, i, params);
-    block.instruction_type = 'tap';
-    block.instructions = tapalong_inst;
-    tapping_blocks{end+1} = block;
-
+    for j=1:3
+        block = stim_maker_omission(strcat(filepath, 'tap_omission_metronome_', all_filewords{i}, '_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), all_periods(i), 25+floor(rand()*5), 0.3, false, i+40, params);
+        block.instruction_type = 'tap';
+        block.instructions = tapalong_inst;
+        tapping_blocks{end+1} = block;
+    end
 end
 
 params.lead_in=0;
@@ -104,34 +111,46 @@ for j = 1:2
     listening_blocks{end+1} = block;
 end
 
-phase_types = {};
-phase_types{1} = struct();
-phase_types{2} = struct();
-phase_types{1}.n_targets = 20;
-phase_types{2}.n_targets = 20;
-phase_types{1}.range = [.5,1.5];
-phase_types{2}.range = [.5,.8];
-phases = {phase_types{1}, phase_types{2}, phase_types{1}, phase_types{2}, phase_types{1}, phase_types{2}};
+intervals = [10, 1];
+identities = [params.target_index, params.target_index];
+[snd_total, code]=master_stim_maker(strcat(filepath, 'free_tap_', j, '_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), intervals, identities, params);
+free_block = struct();
+free_block.sound = snd_total;
+free_block.code = code;
+free_block.type = 'free';
+free_block.instructions = free_inst;
 
-intertrial_range = [.8, 1.5];
-contingency_block = stim_maker_contingency(strcat(filepath, 'contingency_two_phase_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), .5, phases, intertrial_range, 0, params);
-contingency_block.instructions = vertcat( init_rxn_inst, rxn_inst);
-
+% 
+% phase_types = {};
+% phase_types{1} = struct();
+% phase_types{2} = struct();
+% phase_types{1}.n_targets = 20;
+% phase_types{2}.n_targets = 20;
+% phase_types{1}.range = [.5,1.5];
+% phase_types{2}.range = [.5,.8];
+% phases = {phase_types{1}, phase_types{2}, phase_types{1}, phase_types{2}, phase_types{1}, phase_types{2}};
+% 
+% intertrial_range = [.8, 1.5];
+% contingency_block = stim_maker_contingency(strcat(filepath, 'contingency_two_phase_', datestr(clock, 'dd-mmm-yyyy_HH+MM+SS')), .5, phases, intertrial_range, 0, params);
+% contingency_block.instructions = vertcat( init_rxn_inst, rxn_inst);
+% 
 [B,i_listen] = sort(rand(1, length(listening_blocks)));
 for i = 1:length(listening_blocks)
     listening_blocks_shuffled{i} = listening_blocks{i_listen(i)};
 end
+listening_blocks_shuffled{1}.instructions = init_rxn_inst;
+
 [B,i_tap] = sort(rand(1, length(tapping_blocks)));
 for i = 1:length(tapping_blocks)
     tapping_blocks_shuffled{i} = tapping_blocks{i_tap(i)};
 end
 
 
-all_blocks = horzcat(listening_blocks, tapping_blocks, {contingency_block});
-all_blocks_shuffled = horzcat(listening_blocks_shuffled, tapping_blocks_shuffled, {contingency_block});
+all_blocks = horzcat(listening_blocks, free_block, tapping_blocks)%, {contingency_block});
+all_blocks_shuffled = horzcat(listening_blocks_shuffled, free_block, tapping_blocks_shuffled)%, {contingency_block});
 shuffled_indices = horzcat(i_listen, i_tap+length(i_listen), length(all_blocks));
 
-fullsound = init_rxn_inst;
+fullsound = [];
 for i = 1:length(all_blocks)
     fullsound = vertcat(fullsound,all_blocks_shuffled{i}.instructions, all_blocks_shuffled{i}.sound);   
     fullsound = vertcat(fullsound,separation_space);

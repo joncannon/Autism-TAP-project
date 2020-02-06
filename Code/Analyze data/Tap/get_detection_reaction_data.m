3bea9cbcfb23c327186e0472589c68b9cde3b65b
Jonathan <jonathan.j.cannon@gmail.com>
2020-02-06 13:49:33 -0500

    Update get_detection_reaction_data.m

--- Code/Analyze data/Tap/get_detection_reaction_data.m
+++ Code/Analyze data/Tap/get_detection_reaction_data.m
@@ -2,22 +2,21 @@
 % detection/reaction statistics
 
 
-block_struct = all_blocks_w_data
 
 for n = 1:length(all_blocks_w_data)
     % Get info about each tone
-    block_struct{n}.tone_times = block_struct{n}.wav_event_times(mod(block_struct{n}.code, 10)==0);
-    block_struct{n}.tone_volumes = floor(mod(block_struct{n}.code(mod(block_struct{n}.code, 10)==0), 100)/10);
-    block_struct{n}.tone_pitches = floor(mod(block_struct{n}.identities(mod(block_struct{n}.code, 10)==0), 1000)/100);
+    all_blocks_w_data{n}.tone_times = all_blocks_w_data{n}.wav_event_times(mod(all_blocks_w_data{n}.code, 10)==0);
+    all_blocks_w_data{n}.tone_volumes = floor(mod(all_blocks_w_data{n}.code(mod(all_blocks_w_data{n}.code, 10)==0), 100)/10);
+    all_blocks_w_data{n}.tone_pitches = floor(mod(all_blocks_w_data{n}.identities(mod(all_blocks_w_data{n}.code, 10)==0), 1000)/100);
 
     % Get reaction times
-    block_struct{n}.tap_rxn_times = get_asynchronies(block_struct{n}.wav_tap_times, block_struct{n}.tone_times);
+    all_blocks_w_data{n}.tap_rxn_times = get_asynchronies(all_blocks_w_data{n}.wav_tap_times, all_blocks_w_data{n}.tone_times);
 
     % Get more fun info
-    block_struct{n}.undetected_pitches = block_struct{n}.tone_pitches(isnan(block_struct{n}.tap_rxn_times));
-    block_struct{n}.undetected_volumes = block_struct{n}.tone_volumes(isnan(block_struct{n}.tap_rxn_times));
-    block_struct{n}.detected_pitches = block_struct{n}.tone_pitches(~isnan(block_struct{n}.tap_rxn_times));
-    block_struct{n}.detected_volumes = block_struct{n}.tone_volumes(~isnan(block_struct{n}.tap_rxn_times));
-    block_struct{n}.quiet_delays =  block_struct{n}.tap_rxn_times(block_struct{n}.tone_volumes>1 & block_struct{n}.tone_volumes<5 & ~isnan(block_struct{n}.tap_rxn_times));
-    block_struct{n}.loud_delays =  block_struct{n}.tap_rxn_times(block_struct{n}.tone_volumes>4);
+    all_blocks_w_data{n}.undetected_pitches = all_blocks_w_data{n}.tone_pitches(isnan(all_blocks_w_data{n}.tap_rxn_times));
+    all_blocks_w_data{n}.undetected_volumes = all_blocks_w_data{n}.tone_volumes(isnan(all_blocks_w_data{n}.tap_rxn_times));
+    all_blocks_w_data{n}.detected_pitches = all_blocks_w_data{n}.tone_pitches(~isnan(all_blocks_w_data{n}.tap_rxn_times));
+    all_blocks_w_data{n}.detected_volumes = all_blocks_w_data{n}.tone_volumes(~isnan(all_blocks_w_data{n}.tap_rxn_times));
+    all_blocks_w_data{n}.quiet_delays =  all_blocks_w_data{n}.tap_rxn_times(all_blocks_w_data{n}.tone_volumes>1 & all_blocks_w_data{n}.tone_volumes<5 & ~isnan(all_blocks_w_data{n}.tap_rxn_times));
+    all_blocks_w_data{n}.loud_delays =  all_blocks_w_data{n}.tap_rxn_times(all_blocks_w_data{n}.tone_volumes>4);
 end

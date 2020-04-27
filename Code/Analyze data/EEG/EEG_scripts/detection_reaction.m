6074ea12adb124b1385af17c001a435dec18716e
Allen Wang <allenwang@mbp-de-master.lan>
2020-04-27 15:02:05 -0400

    finished detection_reaction script

--- Code/Analyze data/EEG/EEG_scripts/detection_reaction.m
+++ Code/Analyze data/EEG/EEG_scripts/detection_reaction.m
@@ -1,23 +1,42 @@
 % EEGLAB script for detection reaction bins
 % ------------------------------------------------
-% Current prerequisites: bin descriptor, bdf file, consolidated eeg data
+% current prerequisites: bin descriptor, bdf file, consolidated eeg data
 % file, and the script.
-% 
-% This file is a working progresss: goals are to make it more versatile,
-% robust, and more general. At the moment, it takes halely's data from the bdf file to the erp and the plot.
 
-dataset = "Halely_consolidated_eeg_data.mat";
-base_dataset_name = "halely";
-filtered_dataset_name = base_dataset_name + "_filt";
-metronome_frequency = 1.25;
-low_band = 1;
-high_band = 2;
-load(dataset);
+% dialog box to select filter
+prompt = {'Filter out frequencies below :','Filter out frequencies above :'};
+dlgtitle = 'Input';
+dims = [1 100];
+definput = {'0.1','50'};
+answer = inputdlg(prompt,dlgtitle,dims,definput);
+low = str2double(answer(1,1));
+high = str2double(answer(2,1));
 
+% dialog box to select files
+waitfor(msgbox('Please select the bdf file with raw data'))
+[bdffile, bdfpath] = uigetfile('*.bdf');
+waitfor(msgbox('Please select the mat file with consolidated_data'))
+[matfile, matpath] = uigetfile('*.mat');
+waitfor(msgbox('Please select the bin_descriptor file'))
+[binfile, binpath] = uigetfile('*.txt');
+dir = binpath;
+
+% initialize naming of files
+n = strlength(bdffile) - 3;
+base_dataset_name = extractBefore(bdffile,n);
+filtered_dataset_name = strcat(base_dataset_name,'_filt');
+be_dataset_name = strcat(filtered_dataset_name, '_elist_bins_be');
+erp_name = strcat(base_dataset_name, '.erp');
+
+% load mat file
+load(strcat(matpath,matfile));
+
+% creates the dataset
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
-EEG = pop_biosig('/Users/allenwang/Desktop/MATLAB Workspace/Script/halely.bdf', 'ref',[35 36] ,'refoptions',{'keepref' 'off'});
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'setname','halely','gui','off'); 
+EEG = pop_biosig(strcat(bdfpath, bdffile), 'ref',[35 36] ,'refoptions',{'keepref' 'off'});
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 0,'setname',base_dataset_name,'gui','off'); 
 
+% assigns the channels
 try 
     EEG=pop_chanedit(EEG, 'lookup','/Applications/eeglab2019_1/plugins/dipfit/standard_BESA/standard-10-5-cap385.elp','changefield',{41 'type' 'SYNC'},'changefield',{34 'type' 'EYE'},'changefield',{33 'type' 'EYE'});
     [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
@@ -26,21 +45,36 @@ catch
 end
 EEG.event = events_w_taps;
 
-EEG = pop_eegfiltnew(EEG, 'locutoff',0.1,'hicutoff',50,'plotfreqz',1);
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'setname','halely_filt','gui','off'); 
-EEG = pop_eegfiltnew(EEG, 'locutoff',low_band,'hicutoff',high_band,'revfilt',1,'plotfreqz',1);
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 2,'setname','halely_filt_w_band','savenew','halely_filt','gui','off');
+% two filters
+EEG = pop_eegfiltnew(EEG, 'locutoff',low,'hicutoff',high,'plotfreqz',1);
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 1,'setname',filtered_dataset_name,'savenew',filtered_dataset_name,'gui','off'); 
 
-EEG  = pop_creabasiceventlist( EEG , 'BoundaryNumeric', { -99 }, 'BoundaryString', { 'boundary' }, 'Eventlist', '/Users/allenwang/Desktop/MATLAB Workspace/Script/elist.txt' );
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 3,'gui','off'); 
-EEG  = pop_binlister( EEG , 'BDF', '/Users/allenwang/Desktop/MATLAB Workspace/Script/Detection_reaction_bin_descriptor.txt', 'IndexEL',  1, 'SendEL2', 'EEG', 'Voutput', 'EEG' );
+% extract using bin descriptor
+EEG  = pop_creabasiceventlist( EEG , 'BoundaryNumeric', { -99 }, 'BoundaryString', { 'boundary' }, 'Eventlist', strcat(dir, 'elist.txt' ));
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 2,'gui','off'); 
+EEG  = pop_binlister( EEG , 'BDF', strcat(binpath,binfile), 'IndexEL',  1, 'SendEL2', 'EEG', 'Voutput', 'EEG' );
 [ALLEEG EEG] = eeg_store(ALLEEG, EEG, CURRENTSET);
 EEG = pop_epochbin( EEG , [-200.0  800.0],  'pre');
-[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 4,'savenew','halely_filt_w_band_elist_bins_be','gui','off'); 
+[ALLEEG EEG CURRENTSET] = pop_newset(ALLEEG, EEG, 3,'savenew',be_dataset_name,'gui','off'); 
 
-ERP = pop_averager( ALLEEG , 'Criterion', 'good', 'DSindex',  5, 'ExcludeBoundary', 'on', 'SEM', 'on' );
-ERP = pop_savemyerp(ERP, 'erpname','halely', 'filename', 'halely.erp', 'filepath', '/Users/allenwang/Desktop/MATLAB Workspace/Script', 'Warning', 'on');
+% create erp set & do averages
+ERP = pop_averager( ALLEEG , 'Criterion', 'good', 'DSindex',  4, 'ExcludeBoundary', 'on', 'SEM', 'on' );
+ERP = pop_savemyerp(ERP, 'erpname',base_dataset_name, 'filename', erp_name, 'filepath', dir, 'Warning', 'on');
+
+% creates an ERP plot
+ERP = pop_ploterps( ERP,  1, [ 31 32] , 'AutoYlim', 'on', 'Axsize', [ 0.05 0.08], 'BinNum', 'on', 'Blc', 'pre', 'Box', [ 2 1], 'ChLabel',...
+ 'on', 'FontSizeChan',  10, 'FontSizeLeg',  12, 'FontSizeTicks',  10, 'LegPos', 'bottom', 'Linespec', {'k-' }, 'LineWidth',  1, 'Maximize', 'on',...
+ 'Position', [ 103.714 51.6667 106.857 31.9286], 'Style', 'Classic', 'Tag', 'ERP_figure', 'Transparency',  0, 'xscale', [ -199.0 798.0   -100 0:200:600 ],...
+ 'YDir', 'normal' );
+ERP = pop_ploterps( ERP, [ 1 2], [ 31 32] , 'AutoYlim', 'on', 'Axsize', [ 0.05 0.08], 'BinNum', 'on', 'Blc', 'pre', 'Box', [ 2 1], 'ChLabel',...
+ 'on', 'FontSizeChan',  10, 'FontSizeLeg',  12, 'FontSizeTicks',  10, 'LegPos', 'bottom', 'Linespec', {'k-' , 'r-' }, 'LineWidth',  1,...
+ 'Maximize', 'on', 'Position', [ 103.714 51.6667 106.857 31.9286], 'Style', 'Classic', 'Tag', 'ERP_figure', 'Transparency',  0, 'xscale',...
+ [ -199.0 798.0   -100 0:200:600 ], 'YDir', 'normal' );
 ERP = pop_ploterps( ERP, [ 4 5], [ 31 32] , 'AutoYlim', 'on', 'Axsize', [ 0.05 0.08], 'BinNum', 'on', 'Blc', 'pre', 'Box', [ 2 1], 'ChLabel',...
+ 'on', 'FontSizeChan',  10, 'FontSizeLeg',  12, 'FontSizeTicks',  10, 'LegPos', 'bottom', 'Linespec', {'k-' , 'r-' }, 'LineWidth',  1,...
+ 'Maximize', 'on', 'Position', [ 103.714 51.6667 106.857 31.9286], 'Style', 'Classic', 'Tag', 'ERP_figure', 'Transparency',  0, 'xscale',...
+ [ -199.0 798.0   -100 0:200:600 ], 'YDir', 'normal' );
+ERP = pop_ploterps( ERP, [ 1 8], [ 31 32] , 'AutoYlim', 'on', 'Axsize', [ 0.05 0.08], 'BinNum', 'on', 'Blc', 'pre', 'Box', [ 2 1], 'ChLabel',...
  'on', 'FontSizeChan',  10, 'FontSizeLeg',  12, 'FontSizeTicks',  10, 'LegPos', 'bottom', 'Linespec', {'k-' , 'r-' }, 'LineWidth',  1,...
  'Maximize', 'on', 'Position', [ 103.714 51.6667 106.857 31.9286], 'Style', 'Classic', 'Tag', 'ERP_figure', 'Transparency',  0, 'xscale',...
  [ -199.0 798.0   -100 0:200:600 ], 'YDir', 'normal' );
